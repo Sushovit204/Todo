bfbb8f39ea246296cbf10bc456d88a680b150a4c
Sushovit204 <shahsushovit434@gmail.com>
2023-09-13 17:49:33 +0545

    Added the serializers and also added the task model in admin site

---
+++ todo_app/serializers.py
@@ -0,0 +1,19 @@
+from rest_framework import serializers
+from datetime import date
+
+from .models import Task
+
+
+class TaskSerializers(serializers.ModelSerializer):
+    """Creating serializers for the Task Model"""
+
+    class Meta:
+        """Specifying the model and fields on which the serializer is working"""
+        model = Task
+        fields = '__all__'
+
+    def validate_due_date(self, value):
+        """Check if the due date is already gone"""
+        if value < date.today():
+            raise serializers.ValidationError("Due date must be in future")
+        return value
