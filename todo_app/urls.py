fb9d7a5d0a274c81db2baaa43acfbfda054407cd
Sushovit204 <shahsushovit434@gmail.com>
2023-09-15 16:34:31 +0545

    Added the token return view

--- todo_app/urls.py
+++ todo_app/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import TaskListCreateView, TaskDetailView, UserCreate
+from .views import TaskListCreateView, TaskDetailView, UserCreate, LoginView
 
 urlpatterns = [
+    path("login/", LoginView.as_view(), name="login"),
     path('tasks/', TaskListCreateView.as_view(), name='task-list-create'),
     path('tasks/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('users/', UserCreate.as_view(), name='create-user'),
--- todo_app/views.py
+++ todo_app/views.py
@@ -1,5 +1,8 @@
-from django.shortcuts import render
-from rest_framework import generics
+from django.contrib.auth import authenticate
+
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
 
 from .models import Task
 from .serializers import TaskSerializer, UserSerializer
@@ -16,9 +19,23 @@ class TaskDetailView(generics.RetrieveUpdateDestroyAPIView):
     queryset = Task.objects.all()
     serializer_class = TaskSerializer
 
+
 class UserCreate(generics.CreateAPIView):
     """View for creating User"""
     authentication_classes = () #Giving exemption to UserCreate for authentication and permission by overiding global settings
     permission_classes = ()
     serializer_class = UserSerializer
-    
+
+class LoginView(APIView):
+    """View for the user to get token when they have permission"""
+    permission_classes = ()
+
+    def post(self,request):
+        username = request.data.get("username")
+        password = request.data.get("password")
+        user = authenticate(username = username, password = password)
+        if user:
+            return Response({"token": user.auth_token.key})
+        else:
+            return Response({"error": "Wrong Credentials"}, 
+                            status= status.HTTP_400_BAD_REQUEST)
